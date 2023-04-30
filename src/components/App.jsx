0d76b62344a764302b92231b8d261d9f9aadb13d
OksanaDrobakha <6249784@gmail.com>
2023-04-30 11:25:16 +0400

    with mistake

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,78 @@
 import React from 'react';
-import ContactForm from './ContactForm/ContactForm';
+import { ContactForm } from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
+import { Filter } from './Filter/Filter';
+import css from './App.module.css';
 
 export class App extends React.Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  formSubmitHandler = data => {
-    console.log(data);
+  onChangeInput = evt => {
+    const { name, value } = evt.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  addContact = ({ name, number }) => {
+    if (
+      this.state.contacts.some(
+        value => value.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts`);
+    } else {
+      this.setState(prevState => {
+        const list = [...prevState.contacts];
+
+        list.push({
+          id: nanoid(),
+          name: name,
+          number: number,
+        });
+
+        return { contacts: list };
+      });
+    }
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return filteredContacts;
+  };
+
+  onRemove = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id: itemId }) => itemId !== id),
+    }));
   };
 
   render() {
-    return <ContactForm onSubmit={this.formSubmitHandler}></ContactForm>;
+    return (
+      <div className={css.container}>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <ContactList
+          onRemove={this.onRemove}
+          contacts={this.getFilteredContacts()}
+        />
+        <Filter
+          filterValue={this.state.filter}
+          onChangeInput={this.onChangeInput}
+        />
+      </div>
+    );
   }
 }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,53 +2,69 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { object, string, number } from 'yup';
 import { nanoid } from 'nanoid';
-
-const initialValues = {
-  name: '',
-  number: '',
-  id: nanoid(),
-};
-
-let schema = object({
-  name: string().required(),
-  number: number().required().positive().integer(),
-});
-
-const ContactForm = () => {
-  const handleSubmit = (values, { resetForm }) => {
-    console.log(values);
-    resetForm();
+import css from './ContactForm.module.css';
+import PropTypes from 'prop-types';
+export class ContactForm extends React.Component {
+  state = {
+    name: '',
+    number: '',
+    id: nanoid(),
   };
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={schema}
-      onSubmit={handleSubmit}
-    >
-      <Form>
-        <>
-          <label>
-            Name
-            <Field type="text" name="name" />
-            <ErrorMessage name="name" component="div" />
-          </label>
-          <label>
-            Number
-            <Field
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-            <ErrorMessage name="number" component="div" />
-          </label>
-          <button type="submit">Add contact</button>
-        </>
-      </Form>
-    </Formik>
-  );
-};
+  onChangeInput = evt => {
+    const { name, value } = evt.currentTarget;
+    this.setState({ [name]: value });
+  };
 
-export default ContactForm;
+  schema = object({
+    name: string().required(),
+    number: number().required().positive().integer(),
+  });
+
+  resetForm = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <Formik
+        validationSchema={this.schema}
+        onReset={this.resetForm}
+        addContact={this.addContact}
+      >
+        <Form className={css.Form}>
+          <>
+            <label>
+              Name
+              <Field
+                type="text"
+                name="name"
+                onChange={this.onChangeInput}
+                value={this.state.name}
+              />
+              <ErrorMessage name="name" component="div" />
+            </label>
+            <label>
+              Number
+              <Field
+                type="tel"
+                name="number"
+                onChange={this.onChangeInput}
+                value={this.state.number}
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+              />
+              <ErrorMessage name="number" component="div" />
+            </label>
+            <button type="submit">Add contact</button>
+          </>
+        </Form>
+      </Formik>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  addContact: PropTypes.func.isRequired,
+};
